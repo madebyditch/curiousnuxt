ffc684bcdfbebfa7524148514191de36abc91141
Bryce Howitson <brychowitson@gmail.com>
2021-05-11 15:43:17 -0500

    Fix broken Lambda

--- functions/send-emails/send-emails.js
+++ functions/send-emails/send-emails.js
@@ -2,29 +2,26 @@ const axios = require('axios')
 const sgMail = require('@sendgrid/mail')
 
 exports.handler = async function(event, context, callback) {
-    const emails = ["hello@discovercurious.com", "branding@discovercurious.com", "marketing@discovercurious.com", "product@discovercurious.com"];
+    let emails = ["hello@discovercurious.com", "branding@discovercurious.com", "marketing@discovercurious.com", "product@discovercurious.com"];
     const { message, senderEmail, senderName, subject, messageType } = JSON.parse(event.body);
     const slackMsg = {
-        text: `New Contact form submission from ${senderName}. \n ${subject} \n to: ${emails[messageType]}`,
+        text: `New Contact form submission from ${senderName}. \n re: ${subject} \n to: ${emails[messageType]}`
     };
     sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
     const data = {
-        "to": emails[messageType],
+        "to": process.env.SENDGRID_TO_EMAIL,
         "from": {
             "email": process.env.SENDGRID_FROM_EMAIL,
-            "name": senderName
+            "name": "Curious MPLS"
         },
-        "reply_to": {
-            "email": senderName,
-            "name": senderEmail
-        },
-        "subject": "New contact from Curious Website" + subject,
+        "subject": subject,
         "content": [{
             "type": "text/html",
             "value": message
         }]
     };
+
     axios.post(process.env.SLACK_WEBHOOK, JSON.stringify(slackMsg)).then((response) => {
         console.log('SUCCEEDED: Sent slack webhook: \n', response.data);
     }).catch((error) => {});
